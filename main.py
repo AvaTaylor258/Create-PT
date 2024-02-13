25ce73f50186fa068c898d565ce83d3caf18d5a3
Ava Taylor <ava.taylor258@gmail.com>
2024-02-13 10:33:56 -0600

    worked on view_menu()

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import csv
 import os
 
 def add_menu():
+	print("ADDING MENU")
 	task_name = input("Task Name: ")
 	due_date = input("Due Date: ")
 	priority = int(input("Priority: "))
@@ -20,18 +21,28 @@ def add_menu():
 	f.close()
 
 def view_menu():
+	print("VIEWING MENU")
 	f = open("tasks.csv", "r")
-	for i in f:
-		print(i, end="")
+	c = csv.reader(f)
+
+	print()
+	for r in c:
+		if r[0] == "Task Name":
+			pass
+		else:
+			print(f"\t{r[0]}")
+	print()
+	input("Press enter to continue ")
+	
 	f.close()
 
 while True:
-	opt = int(input("Welcome to To-Ultimate-Do\nWhat would you like to do?\n[0] Quit\n[1] Add\n[2] View\n[3] Edit\n> "))
+	os.system("clear")
+	opt = int(input("Welcome to To-Ultimate-Do\nWhat would you like to do?\n[0] Quit\n[1] Add\n[2] View\n> "))
+	os.system("clear")
 	if opt == 0:
 		break
 	elif opt == 1:
 		add_menu()
 	elif opt == 2:
 		view_menu()
-	elif opt == 3:
-		print("edit")
